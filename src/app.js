ab8c5a2bb68316d8463536cdf76c13b77ba72467
Eduardo-correa1 <159133054+Eduardo-correa1@users.noreply.github.com>
2024-05-10 19:05:35 +0000

    cards

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ["♦", "♥", "♠", "♣"];
+  const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  function paloAleatorio() {
+    const obtenerPalo = Math.floor(Math.random() * palos.length);
+    return palos[obtenerPalo];
+  }
+
+  const palo = paloAleatorio();
+
+  const elementosPalo = document.getElementsByClassName("palo");
+
+  for (let i = 0; i < elementosPalo.length; i++) {
+    elementosPalo[i].textContent = palo;
+    if (palo === "♥" || palo === "♦") {
+      elementosPalo[i].classList.add("rojo");
+    }
+  }
+
+  function valorAleatorio() {
+    const obtenerValor = Math.floor(Math.random() * valores.length);
+    return valores[obtenerValor];
+  }
+  const numero = valorAleatorio();
+  const elementosNumero = document.getElementsByClassName("numero");
+  for (let i = 0; i < elementosNumero.length; i++) {
+    elementosNumero[i].textContent = numero;
+  }
 };
